baeaa155f23ddca5157a0d549d5e1b819c3400a0
Nehemy <nehemynet@gmail.com>
2025-02-21 15:12:48 +0100

    Implemented role-based access control (RBAC) in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
     
@@ -39,3 +40,28 @@ class CustomLoginView(LoginView):
 
 def logout_view(request):
     return render(request, 'logout.html')
+
+
+def is_admin(user):
+    return user.is_authenticated and user.profile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.profile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.profile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
