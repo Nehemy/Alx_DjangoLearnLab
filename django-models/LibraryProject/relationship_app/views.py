c86c1c966d9e54e7880a4795b78058e6f91dd38d
Nehemy <nehemynet@gmail.com>
2025-02-21 15:01:49 +0100

    Fixed URL patterns for authentication views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -21,7 +21,7 @@ class RegisterView(CreateView):
     form_class = UserCreationForm
     template_name = 'register.html'
     success_url = reverse_lazy('login')
-    
+
     def form_valid(self, form):
         user = form.save()
         login(self.request, user)
