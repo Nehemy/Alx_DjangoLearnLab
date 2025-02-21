bf368104172205c2a3905cfba2e2fdae5ffcd5f6
Nehemy <nehemynet@gmail.com>
2025-02-21 13:35:54 +0100

    commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,17 +1,10 @@
-'''
-Prepare a Python script query_samples.py in the relationship_app directory. This script should contain the query for each of the following of relationship:
-Query all books by a specific author.
-List all books in a library.
-Retrieve the librarian for a library.
-'''
+from .models import Book, Library, Librarian, Author
 
-from .models import Book, Library, Librarian
+author = Author.objects.get(name='author_name')
+books_by_author = Book.objects.filter(author=author)
 
-books = Book.objects.get(author="Name")
-print(books.author)
-
-books = Library.objects.get(name=library_name)
+books = Library.objects.get(name='library_name')
 print(books.all())
 
-librarian = Librarian.objects.all()
-print(librarian.library)
+library = Library.objects.get(name='library_name')
+librarian = library.librarian
