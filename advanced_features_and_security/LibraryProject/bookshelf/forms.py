c557320913a06fe8dafb05f1b5b64f3ad2a1c689
Nehemy <nehemynet@gmail.com>
2025-02-25 13:44:39 +0100

    Added ExampleForm to bookshelf/forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import Book
 
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=200, required=True, help_text="Enter the book title")
+    author = forms.CharField(max_length=100, required=True, help_text="Enter the author's name")
+
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
