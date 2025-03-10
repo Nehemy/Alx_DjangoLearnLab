0616fcb14487db3d0f0776201af38b1cb177396a
Nehemy <nehemynet@gmail.com>
2025-03-10 14:09:07 +0100

    third task

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, filters
+from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import BookSerializer
 from .models import Book
