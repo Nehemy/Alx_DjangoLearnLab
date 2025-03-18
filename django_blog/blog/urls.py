063ffcd1a8b712bcdf1051e6462af0579d91883e
Nehemy <nehemynet@gmail.com>
2025-03-18 15:00:04 +0100

    Task 1 done

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', index, name='home'),
     path('posts/', index, name='posts'),
     
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logged_out.html'), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change_form.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
