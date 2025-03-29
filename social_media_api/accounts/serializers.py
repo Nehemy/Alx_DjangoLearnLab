e5003c03cac5f56a15c009ad5afb2b26d3067dfc
Nehemy <nehemynet@gmail.com>
2025-03-29 14:32:52 +0100

    Task 0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -13,7 +13,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ('username', 'password', 'email', 'bio', 'profile_picture')
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
-        Token.objects.create(user=user)
         return user
 
 class ProfileSerializer(serializers.ModelSerializer):
