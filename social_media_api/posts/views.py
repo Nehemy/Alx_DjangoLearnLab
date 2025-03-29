4921dd4852be22b90f6eeb76efd30729fb927ff6
Nehemy <nehemynet@gmail.com>
2025-03-29 15:16:42 +0100

    Task3

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,8 +1,11 @@
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics, permissions, status
 from rest_framework.filters import SearchFilter
-from .models import Post, Comment
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+from notifications.models import Notification
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -22,3 +25,26 @@ class FeedView(generics.ListAPIView):
     def get_queryset(self):
          following_users = self.request.user.following.all()
          return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+class LikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request, pk, *args, **kwargs):
+         post = get_object_or_404(Post, pk=pk)
+         user = request.user
+         if Like.objects.filter(user=user, post=post).exists():
+             return Response({'detail': 'Already liked.'}, status=status.HTTP_400_BAD_REQUEST)
+         Like.objects.create(user=user, post=post)
+         if post.author != user:
+             Notification.objects.create(recipient=post.author, actor=user, verb='liked', target=post)
+         return Response({'detail': 'Post liked.'}, status=status.HTTP_200_OK)
+
+class UnlikePostView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request, pk, *args, **kwargs):
+         post = get_object_or_404(Post, pk=pk)
+         user = request.user
+         like = Like.objects.filter(user=user, post=post).first()
+         if not like:
+             return Response({'detail': 'Not liked yet.'}, status=status.HTTP_400_BAD_REQUEST)
+         like.delete()
+         return Response({'detail': 'Post unliked.'}, status=status.HTTP_200_OK)
