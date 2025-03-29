3f98349760417e1c4a6cba6eff604e2e328c05fd
Nehemy <nehemynet@gmail.com>
2025-03-29 15:02:07 +0100

    Task 2

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -2,17 +2,15 @@ from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
-from django.contrib.auth import get_user_model
+from .models import CustomUser
 from .serializers import RegistrationSerializer, ProfileSerializer
 
-User = get_user_model()
-
 class RegistrationView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = RegistrationSerializer
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
-        user = User.objects.get(username=response.data['username'])
+        user = CustomUser.objects.get(username=response.data['username'])
         token, created = Token.objects.get_or_create(user=user)
         response.data['token'] = token.key
         return response
@@ -26,7 +24,7 @@ class LoginView(ObtainAuthToken):
         return Response({'token': token.key})
 
 class ProfileView(generics.RetrieveUpdateAPIView):
-    queryset = User.objects.all()
+    queryset = CustomUser.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [permissions.IsAuthenticated]
     def get_object(self):
@@ -36,8 +34,8 @@ class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
     def post(self, request, user_id, *args, **kwargs):
         try:
-            user_to_follow = User.objects.get(id=user_id)
-        except User.DoesNotExist:
+            user_to_follow = CustomUser.objects.get(id=user_id)
+        except CustomUser.DoesNotExist:
             return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
         request.user.following.add(user_to_follow)
         return Response({'detail': 'Followed successfully.'}, status=status.HTTP_200_OK)
@@ -46,8 +44,8 @@ class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
     def post(self, request, user_id, *args, **kwargs):
         try:
-            user_to_unfollow = User.objects.get(id=user_id)
-        except User.DoesNotExist:
+            user_to_unfollow = CustomUser.objects.get(id=user_id)
+        except CustomUser.DoesNotExist:
             return Response({'detail': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
         request.user.following.remove(user_to_unfollow)
         return Response({'detail': 'Unfollowed successfully.'}, status=status.HTTP_200_OK)
