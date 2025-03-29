790116a010596fd1cd9673e6bc01dcd247f5b771
Nehemy <nehemynet@gmail.com>
2025-03-29 14:45:06 +0100

    Task 1

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,7 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     followers = models.ManyToManyField('self', symmetrical=False, blank=True)
+    groups = models.ManyToManyField(
+        Group,
+        blank=True,
+        related_name='customuser_set',
+        help_text='The groups this user belongs to.'
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        blank=True,
+        related_name='customuser_set',
+        help_text='Specific permissions for this user.'
+    )
