3d0cb8d40cbe071bbb2edba7f9e8afc443527b20
Nehemy <nehemynet@gmail.com>
2025-03-29 14:35:39 +0100

    Task 0

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -9,14 +9,14 @@ User = get_user_model()
 class RegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
-        model = get_user_model()
-        fields = ('username', 'password', 'email', 'bio', 'profile_picture')
+        model = User
+        fields = ['username', 'password', 'email', 'bio', 'profile_picture']
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
-        fields = ('username', 'email', 'bio', 'profile_picture', 'followers')
+        model = User
+        fields = ['username', 'email', 'bio', 'profile_picture', 'followers']
